dd0d4be21943b49cc7c202b55989f1f7d84e97a9
PorcaM <tjdwns5122@gmail.com>
2016-08-11 16:45:59 +0900

    Skill v0.3

--- parts/Skill.cpp
+++ parts/Skill.cpp
@@ -1,8 +1,9 @@
 #include "Skill.h"
-Skill::Skill(string _name, Skill *_parent, float _cooltime) {
+Skill::Skill(string _name, Skill *_parent, float _cooltime, int _type) {
 	setName(_name);
 	setParent(_parent);
 	setCooltime(_cooltime);
+	setType(_type);
 }
 
 void Skill::setName(string _name) {
@@ -16,6 +17,9 @@ void Skill::setCooltime(float _cooltime) {
 		_cooltime = 0;
 	cooltime = _cooltime;
 }
+void Skill::setType(int _type) {
+	type = _type;
+}
 string Skill::getName() {
 	return name;
 }
@@ -24,4 +28,7 @@ Skill *Skill::getParent() {
 }
 float Skill::getCooltime() {
 	return cooltime;
+}
+int Skill::getType() {
+	return type;
 }
