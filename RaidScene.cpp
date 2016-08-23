4da72eda374a3275b2e9b8d72de715d69564ab34
PorcaM <tjdwns5122@gmail.com>
2016-08-23 16:47:13 +0900

    raid visual complete

--- RaidScene.cpp
+++ RaidScene.cpp
@@ -5,6 +5,7 @@ Revision:	2016/08/23 by PorcaM
 
 #include "RaidScene.h"
 #include "characters\Character.h"
+#include "skillinstance\SkillFactory.h"
 
 USING_NS_CC;
 
@@ -56,15 +57,45 @@ bool Raid::init()
 
 	/////////////////////////////
 	// 3. add your codes below
-	Character *cl[6];
 	Tier tempT = Tier(1);
 	string tl[6] = { "MeleeNPC", "Monster", "TankNPC", "MeleeNPC", "MeleeNPC", "RangeNPC" };
 	string rl[6] = { "melee.png", "monster.png", "tank.png", "melee.png", "melee.png", "range.png" };
-	for (int i = 0; i < 6; i++) {
-		cl[i] = Character::create(rl[i], tempT, tl[i], 5);
-		this->addChild(cl[i]);
-	}
 
+	auto playerLayer = Layer::create();
+	this->addChild(playerLayer);
+	Character *cl[6];
+	for (int i = 0; i < 6; i++) {
+		if (i == 1) continue;
+		cl[i] = Character::create(rl[i], tempT, tl[i], 5);
+		cl[i]->setPosition(Vec2(0, -100 * i));
+		cl[i]->setScale(0.5f);
+		cl[i]->setAnchorPoint(Vec2(0.5, 0.5));
+		playerLayer->addChild(cl[i]);
+	}
+	playerLayer->setPosition(200, 600);
+
+	auto bossLayer		= Layer::create();
+	this->addChild(bossLayer);
+	cl[1] = Character::create(rl[1], tempT, tl[1], 5);
+	cl[1]->setPosition(Vec2(0, 0));
+	cl[1]->setScale(1.0f);
+	cl[1]->setAnchorPoint(Vec2(0.5, 0.5));
+	bossLayer->addChild(cl[1]);
+	bossLayer->setPosition(Vec2(visibleSize.width / 2, visibleSize.height-150));
+
+	auto skillLayer = Layer::create();
+	this->addChild(skillLayer);
+	Skill *sl[5];
+	for (int i = 0; i < 5; i++) {
+		sl[i] = SkillFactory::getSkill(heal);
+		sl[i]->printInfo();
+		sl[i]->initWithFile("images/skill2.png");
+		sl[i]->setPosition(Vec2(0, -100 * i));
+		sl[i]->setAnchorPoint(Vec2(0.5, 0.5));
+		sl[i]->setScale(0.5f);
+		skillLayer->addChild(sl[i]);
+	}
+	skillLayer->setPosition(Vec2(400, 600));
 
 	return true;
 }
