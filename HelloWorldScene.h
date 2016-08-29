f99d5e69684e641961ef4220261763d271478ae8
kukania <angkswnstn@naver.com>
2016-08-29 16:16:32 +0900

    index 0.5

--- HelloWorldScene.h
+++ HelloWorldScene.h
@@ -2,14 +2,20 @@
 #define __HELLOWORLD_SCENE_H__
 
 #include "cocos2d.h"
-
+#include"ui/CocosGUI.h"
+#include<vector>
 USING_NS_CC;
+
+#define MENUBTN 4
 class HelloWorld : public cocos2d::Layer
 {
 public:
+	std::vector<Vec2>positionArr;
+	ui::ScrollView* scrollView;
 	DrawNode* backGround;
 	cocos2d::Layer* characterGroup;
 	DrawNode* statusHexa;
+	bool menuBtnTouched;
     static cocos2d::Scene* createScene();
 	void makeBackGround();
 	bool checkCharacterGroup(Point);
@@ -19,6 +25,9 @@ public:
     // a selector callback
     void menuCloseCallback(cocos2d::Ref* pSender);
 	virtual bool onTouchBegan(Touch* touch, Event*);
+	virtual void onTouchMoved(Touch *touch, Event*);
+	virtual void onTouchEnded(Touch *touch, Event*);
+
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
 };
