47054ac2acbf97b095573eb1defcff090ec4e20e
PorcaM <tjdwns5122@gmail.com>
2016-10-04 09:37:22 +0900

    refac skillnode nc

--- skilltree/SkillTree.cpp
+++ skilltree/SkillTree.cpp
@@ -90,22 +90,22 @@ void SkillTree::InitHealSkillTree(){
 	SkillNode *temp;
 	insertSkill(new SkillNode (0, 0, true));
 	insertSkill(temp = new SkillNode (1, 0, false));
-	temp->set_col_n_row(0, 0);
+	temp->set_col_row(0, 0);
 	insertSkill(temp = new SkillNode (8, 1, false));
-	temp->set_col_n_row(0, 1);
+	temp->set_col_row(0, 1);
 	insertSkill(temp = new SkillNode (15, 1, false));
-	temp->set_col_n_row(0, 2);
+	temp->set_col_row(0, 2);
 	for (int i = 0; i < 6; ++i){  // from 2 to 7
 		insertSkill(temp = new SkillNode (2+i, 1+i, false));
-		temp->set_col_n_row(i+1, 0);
+		temp->set_col_row(i+1, 0);
 	}
 	for (int i = 0; i < 6; ++i){  // from 9 to 14
 		insertSkill(temp = new SkillNode (9+i, 8+i, false));
-		temp->set_col_n_row(i+1, 1);
+		temp->set_col_row(i+1, 1);
 	}
 	for (int i = 0; i < 6; ++i){  // from 16 to 21
 		insertSkill(temp = new SkillNode (16+i, 8+i, false));
-		temp->set_col_n_row(i+1, 2);
+		temp->set_col_row(i+1, 2);
 	}
 	return;
 }
